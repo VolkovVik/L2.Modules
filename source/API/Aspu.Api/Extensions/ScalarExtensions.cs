4eb60d398ec691e82cf9c3e79eb6e68be37c96c0
viktor.volkov <volkov.vik1@gmail.com>
2025-12-09 18:54:41 +0300

    feat. Add scalar extensions

---
+++ source/API/Aspu.Api/Extensions/ScalarExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+using Scalar.AspNetCore;
+
+namespace Aspu.Api.Extensions;
+
+internal static class ScalarExtensions
+{
+    internal static IServiceCollection AddOpenApi(this IServiceCollection services, int[] versions)
+    {
+        foreach (var version in versions)
+            services.AddOpenApi($"v{version}", options => GetOptions(options, $"ASPU API Reference version {version}"));
+
+        return services;
+    }
+
+    private static void GetOptions(OpenApiOptions options, string title)
+    {
+        options.AddDocumentTransformer((document, context, cancellationToken) =>
+        {
+            document.Info.Version = "0.0.1";
+            document.Info.Title = title;
+            document.Info.Description = "This API demonstrates OpenAPI customization in a .NET 9 ASPU project.";
+            document.Info.TermsOfService = new Uri("https://vekas-automation.ru/");
+            document.Info.Contact = new OpenApiContact
+            {
+                Name = "Vekas",
+                Email = "info@vekas-automation.ru",
+                Url = new Uri("https://vekas-automation.ru/")
+            };
+            return Task.CompletedTask;
+        });
+    }
+
+
+    internal static IEndpointRouteBuilder MapScalarApiReference(this IEndpointRouteBuilder endpoints, int[] versions)
+    {
+        endpoints.MapScalarApiReference(options =>
+        {
+            options.WithTitle("ASPU API Reference")
+                .WithTheme(ScalarTheme.BluePlanet)
+                .AddDocuments(versions.Select(x => $"v{x}"))
+                .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+        });
+        return endpoints;
+    }
+}
--- source/API/Aspu.Api/Program.cs
+++ source/API/Aspu.Api/Program.cs
@@ -1,17 +1,17 @@
 using System.Reflection;
+using Aspu.Api.Extensions;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+int[] versions = [1, 2];
+builder.Services.AddOpenApi(versions);
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(versions);
 }
 
 app.UseHttpsRedirection();
