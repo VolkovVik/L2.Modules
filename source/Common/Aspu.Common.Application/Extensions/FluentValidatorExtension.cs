d79305ff7010d2113cbbac86baa4d138d55afc8b
viktor.volkov <volkov.vik1@gmail.com>
2026-03-08 17:04:17 +0300

    feat. Add Aspu.Common projects

--- source/Common/Aspu.Common.Application/Extensions/FluentValidatorExtension.cs
+++ source/Common/Aspu.Common.Application/Extensions/FluentValidatorExtension.cs
@@ -8,5 +8,5 @@ public static class FluentValidatorExtension
         ruleBuilder.Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage("The value is null or empty");
 
     public static IRuleBuilderOptions<T, IList<TElement>> ListMustContainFewerThan<T, TElement>(this IRuleBuilder<T, IList<TElement>> ruleBuilder, int num) =>
-        ruleBuilder.Must(list => list.Count < num).WithMessage("The list contains too many items");
+        ruleBuilder.Must(list => list is not null && list.Count < num).WithMessage("The list contains too many items");
 }
